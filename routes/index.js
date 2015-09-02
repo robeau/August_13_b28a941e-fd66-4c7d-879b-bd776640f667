9540b4ec17b963fecdce30cf79102c5599a9fb37
natalie <brandnew03@gmail.com>
2015-09-02 15:14:57 -0700

    Updated view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var uuid = require('node-uuid');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    uuid: uuid
+  });
 });
 
 module.exports = router;
